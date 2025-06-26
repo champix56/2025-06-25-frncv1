b254f26aac9be4bda61c3e29604516c7b5c5430c
champix56 <desorbaix@free.fr>
2025-06-26 12:51:57 +0200

    gestion formulaire complexe

--- src/components/functional/EditProduct/EditProduct.tsx
+++ src/components/functional/EditProduct/EditProduct.tsx
@@ -1,27 +1,104 @@
-import { Button, Image, Switch, Text, TextInput, View } from "react-native";
+import { Button, Image, Platform, Switch, Text, TextInput, View } from "react-native";
 import React, { useEffect, useState } from "react";
 import styles from "./EditProduct.styles";
-interface IEditProductProps {}
+import IProduct from "../../../interfaces/iproduct";
+interface IEditProductProps {
+  product: IProduct;
+  onProductChange: Function;
+}
 interface IEditProductState {}
 const editProductInitialState = {};
 const EditProduct = (props: IEditProductProps) => {
-  const [state, setstate] = useState<IEditProductState>(
-    editProductInitialState
-  );
+  useEffect(() => {
+    setProduct(props.product);
+  }, [props.product]);
+
+  const [product, setProduct] = useState<IProduct>({
+    description: "",
+    image: "",
+    name: "",
+    prix: 0,
+    stock: 0,
+  });
+  // const [image, setImage] = useState("");
+  // useEffect(() => {
+
+  // }, [image])
   useEffect(() => {
     return () => {};
   }, []);
+  function onTextChange(
+    propertyName: string,
+    value: string,
+    type: "string" | "number" = "string"
+  ) {
+    const tmp = { ...product };
+    switch (type) {
+      case "number":
+        tmp[propertyName] = Number(value);
+        break;
+      default:
+        tmp[propertyName] = value;
+    }
+    setProduct(tmp);
+  }
   return (
     <View style={styles.EditProduct}>
       <Text>Image du produit</Text>
-      <Image style={styles.image} source={{uri:"https://www.ioburo.fr/453413-large_default/carton-de-demenagement-55x35.jpg"}}/>
-      <TextInput placeholder="Titre du produit" />
-      <TextInput placeholder="0.00€" />
-      <TextInput placeholder="chemin image" />
+      <Image
+        style={styles.image}
+        source={{
+          uri:
+            product.image.length > 0
+              ? product.image
+              : "https://www.ioburo.fr/453413-large_default/carton-de-demenagement-55x35.jpg",
+        }}
+      />
+      <TextInput
+        placeholder="Titre du produit"
+        value={product.name}
+        onChangeText={(t) => {
+          onTextChange("name", t);
+        }}
+      />
+      <TextInput
+        placeholder="0.00€" keyboardType="numeric"
+        value={product.prix.toFixed(2)}
+        onChangeText={(t) => {
+          onTextChange("prix", t, "number");
+        }}
+      />
+      <TextInput
+        placeholder="chemin image"
+        value={product.image}
+        onChangeText={(t) => {
+          onTextChange("image", t);
+        }}
+      />
       <Text>Disponible</Text>
-      <Switch/>
-      <Button title="Annuler"/>
-      <Button title="Enregistrer"/>
+      <Switch
+        value={product.stock > 0}
+        onValueChange={(b) => {
+          onTextChange("stock", Number(b).toString(), "number");
+        }}
+      />
+      <TextInput
+        placeholder="description"
+        value={product.description}
+        onChangeText={(t) => {
+          onTextChange("description", t);
+        }}
+        keyboardType={Platform.OS==="ios"?"name-phone-pad":"decimal-pad"}
+      />
+      <Button title="Annuler" />
+      <Button
+        title="Enregistrer"
+        onPress={() => {
+          props.onProductChange(product);
+        }}
+      />
+      {Platform.OS==='ios'&&<Text>IOS</Text>}
+      <Text>{JSON.stringify(product)}</Text>
     </View>
   );
 };
