e89710cd76b6d39fefbce3a2b7acb34ab20550f2
champix56 <desorbaix@free.fr>
2025-06-27 12:01:18 +0200

    propag des props parent dun cmp connecté

--- src/components/functional/EditProduct/EditProduct.store.tsx
+++ src/components/functional/EditProduct/EditProduct.store.tsx
@@ -4,13 +4,16 @@ import Editor from "./EditProduct";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../store/store";
 import { saveProductToServer } from "../../../store/asyncCalling";
-type Props = {};
+type Props = {
+  style?:{}
+};
 
 const EditProduct = (props: Props) => {
   const prod = useSelector((storeState: RootState) => storeState.current);
   const dispatch: AppDispatch = useDispatch();
   return (
     <Editor
+      {...props}
       onProductChange={(p) => {
         dispatch(saveProductToServer(p));
       }}
